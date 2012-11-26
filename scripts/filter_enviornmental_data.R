c3e6470932cbdc76723c9c6aac83aba335ce1992
dmcglinn <danmcglinn@gmail.com>
2012-11-25 23:54:11 -0700

    Added additional data massaging routines for the enviornmental dataset

--- scripts/filter_enviornmental_data.R
+++ scripts/filter_enviornmental_data.R
@@ -1,8 +1,31 @@
 setwd('~/Lab data/tgp_management/')
 
 env = read.csv('./data/tgpall_env.csv')
+env$date_samp = as.Date(env$date_samp, format='%m/%d/%Y')
+env$date_of_burn = as.Date(env$date_of_burn, format='%m/%d/%Y')
+
 ## drop the plots not on the UTM grid
 env = subset(env, subset=grid_plot == 1)
+
+soilvars = c("P","CA","MG","K","NA.","B","FE","MN","CU","ZN","AL")
+## add 1.0 to Boron so that there are no zero values
+env$B = env$B + 1
+logSoil = log10(env[ , soilvars])
+names(logSoil) = paste('log', soilvars, sep='')
+
+eastness = sin(env$aspect) 
+northness = cos(env$aspect)
+
+woody_vars = c('woodyht', 'woodypct', 'basal_area', 'density', 'avgdens')
+env[ , woody_vars] = ifelse(is.na(as.matrix(env[ , woody_vars])), 
+                            0, 
+                            as.matrix(env[ , woody_vars]))
+
+env = env[ , !(names(env) %in% soilvars)]
+env = cbind(env, logSoil, eastness, northness)
+
+## write out filtered env file
+write.csv(env, file='./data/tgp_utm_env.csv', row.names=F)
 
 plot(env$easting, env$northing)
 plot(env$easting, env$northing, cex = log10(env$woodypct))
