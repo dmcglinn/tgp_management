716a3c51d12a36471e64474e4e9d885ca289969b
Daniel McGlinn <danmcglinn@gmail.com>
2013-05-17 14:54:42 -0600

    Add data prep and additional variation partitioning analysis

--- scripts/tgp_run_all.R
+++ scripts/tgp_run_all.R
@@ -1,10 +1,21 @@
 
-setwd('~/tgp_management/')
+setwd('~/tgp_management/scripts')
 
-dir.create('./results')
+dir.create('../results')
 dir.create('./log_files')
 
-## variation partitioning of repeat plots
+## perpare datasets ------------------------------------------------------------
 
+system('Rscript tgp_mesonet_download.R > ./log_files/mesonet_download.log 2>&1',
+       wait=FALSE)
+
+
+## conduct analysis ------------------------------------------------------------
+## variation partitioning of grid plots
+system('Rscript tgp_grid_varpart.R > ./log_files/grid_varpart.log 2>&1', 
+       wait=FALSE)
+
+## variation partitioning of repeat plots
 system('Rscript tgp_repeat_varpart.R > ./log_files/repeat_varpart.log 2>&1', 
        wait=FALSE)
+
