0250e2bcb8c9ee4b7f2416dbfa8ab34b1d17d4a9
Daniel McGlinn <danmcglinn@gmail.com>
2013-05-17 13:35:48 -0600

    Script for downloading and computing monthly average on mesonet data at preserve

---
+++ scripts/tgp_mesonet_download.R
@@ -0,0 +1,8 @@
+library(okmesonet)
+
+fora.mts = okmts(begintime="1996-06-01 00:00:00",
+                 endtime="2009-05-31 23:55", station="fora")
+
+fora.mts.avg = avgokmts(fora.mts, by="month")
+
+write.csv(fora.mts.avg, file='../data/mesonet_mo_avg.csv', row.names=F)
