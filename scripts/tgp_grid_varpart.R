c9cc24dbc3833211db5f5c6aa57364a01292c6a0
Daniel McGlinn <danmcglinn@gmail.com>
2013-05-17 19:57:44 -0600

    Script for carrying out variation partitioning on the grid dataset

---
+++ scripts/tgp_grid_varpart.R
@@ -0,0 +1,24 @@
+library(vegan)
+
+setwd('~/tgp_management/')
+
+source('./scripts/tgp_functions.R')
+source('./scripts/tgp_grid_data_import.R')
+
+dir.create('./results/')
+
+## carry out partitioning analysis
+ols_part = ordi_part(env$sr, soil_mat, mang_mat, method='rda')
+
+rda_part = ordi_part(comm_sqr, soil_mat, mang_mat, method='rda')
+
+cca_part = ordi_part(comm_sqr, soil_mat, mang_mat, method='cca', nperm=999)
+
+## print results
+ols_part$part
+
+rda_part$part
+
+cca_part$part
+
+save(ols_part, rda_part, cca_part, file='./results/grid_varpart.Rdata')
