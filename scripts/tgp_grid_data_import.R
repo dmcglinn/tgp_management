022854bf76dbf9be24e3aeac431a276774e32154
Daniel McGlinn <danmcglinn@gmail.com>
2013-05-21 10:15:50 -0600

    drop setting of working directory and reformat for style

--- scripts/tgp_grid_data_import.R
+++ scripts/tgp_grid_data_import.R
@@ -1,4 +1,3 @@
-setwd('~/tgp_management/')
 
 env = read.csv('./data/tgp_utm_env_complete.csv')
 comm = read.csv('./data/tgp_comm_mat_all.csv')
@@ -8,8 +7,8 @@ env[is.na(env$waterpct), ]$waterpct = 0 ## checked coordinate
 env[is.na(env$rockpct), ]$rockpct = 0
 
 grassland = env$waterpct == 0 &
-  env$rockpct <= 20 &
-  env$woodypct <= 20
+            env$rockpct <= 20 &
+            env$woodypct <= 20
 env = env[grassland, ]
 
 pl_yr = env$plot_yr[env$repeat_plot == 0]
