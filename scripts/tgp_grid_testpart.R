b7eb0471f6723873f4bd516712109351bb943058
Daniel McGlinn <danmcglinn@gmail.com>
2013-05-21 14:56:27 -0600

    Removed redundant tests

--- scripts/tgp_grid_testpart.R
+++ scripts/tgp_grid_testpart.R
@@ -12,50 +12,23 @@ print('OLS model of richness------------------------------------')
 ## ols model of richness
 full = rda(env$sr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
              mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
-soil_ind = rda(env$sr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
-                 Condition(mang_mat$YrsOB) + Condition(mang_mat$BP5Yrs) + Condition(mang_mat$YrsSLB))
-mang_ind = rda(env$sr ~ 
-                 Condition(soil_mat[,1]) + Condition(soil_mat[,2]) + Condition(soil_mat[,3]) +
-                 mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
-
 summary(lm(env$sr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
              mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB))
 
 anova(full, by='margin')
-anova(soil_ind, by='margin')
-anova(mang_ind, by='margin')
 
 print('RDA model of composition-------------------------------')
 ### rda model of composition
 full = rda(comm_sqr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
              mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
-soil_ind = rda(comm_sqr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
-                 Condition(mang_mat$YrsOB) + Condition(mang_mat$BP5Yrs) + Condition(mang_mat$YrsSLB))
-mang_ind = rda(comm_sqr ~ 
-                 Condition(soil_mat[,1]) + Condition(soil_mat[,2]) + Condition(soil_mat[,3]) +
-                 mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
-
 anova(full, by='margin')
-anova(soil_ind, by='margin')
-anova(mang_ind, by='margin')
 
 print('CCA model of composition-------------------------------')
 ### cca model of composition
 full = cca(comm_sqr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
              mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
-soil_ind = cca(comm_sqr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
-                 Condition(mang_mat$YrsOB) + Condition(mang_mat$BP5Yrs) + Condition(mang_mat$YrsSLB))
-rain_ind = cca(comm_sqr ~ 
-                 Condition(soil_mat[,1]) + Condition(soil_mat[,2]) + Condition(soil_mat[,3]) +
-                 Condition(mang_mat$YrsOB) + Condition(mang_mat$BP5Yrs) + Condition(mang_mat$YrsSLB))
-mang_ind = cca(comm_sqr ~ 
-                 Condition(soil_mat[,1]) + Condition(soil_mat[,2]) + Condition(soil_mat[,3]) +
-                 mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
 
 anova(full, by='margin')
-anova(soil_ind, by='margin')
-anova(mang_ind, by='margin')
-
 
 ## examine for residual spatial dependence
 tgp_xy = env[ , c('easting', 'northing')]
@@ -69,7 +42,7 @@ rda_mso = mso(rda_lm, tgp_xy, grain=1000, permutations = 999)
 cca_mso = mso(cca_lm, tgp_xy, grain=1000, permutations = 999)
 
 pdf('./figs/grid_model_mso.pdf')
-  msoplot(ols_mso, main='Grid, OLS')
-  msoplot(rda_mso, main='Grid, RDA')
-  msoplot(cca_mso, main='Grid, CCA')
+  msoplot(ols_mso, main='Grid, OLS', ylim=c(50, 250))
+  msoplot(rda_mso, main='Grid, RDA', ylim=c(15, 30))
+  msoplot(cca_mso, main='Grid, CCA', ylim=c(3, 6))
 dev.off()
--- scripts/tgp_repeat_testpart.R
+++ scripts/tgp_repeat_testpart.R
@@ -14,71 +14,28 @@ print('OLS model of richness-----------------------------------')
 full = rda(env$sr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
                     rain_mat$sum_rain + rain_mat$win_rain + rain_mat$spr_rain +
                     mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
-soil_ind = rda(env$sr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
-               Condition(rain_mat$sum_rain) + Condition(rain_mat$win_rain) + Condition(rain_mat$spr_rain) +
-               Condition(mang_mat$YrsOB) + Condition(mang_mat$BP5Yrs) + Condition(mang_mat$YrsSLB))
-rain_ind = rda(env$sr ~ 
-                 Condition(soil_mat[,1]) + Condition(soil_mat[,2]) + Condition(soil_mat[,3]) +
-                 rain_mat$sum_rain + rain_mat$win_rain + rain_mat$spr_rain +
-                 Condition(mang_mat$YrsOB) + Condition(mang_mat$BP5Yrs) + Condition(mang_mat$YrsSLB))
-mang_ind = rda(env$sr ~ 
-                 Condition(soil_mat[,1]) + Condition(soil_mat[,2]) + Condition(soil_mat[,3]) +
-                 Condition(rain_mat$sum_rain) + Condition(rain_mat$win_rain) + Condition(rain_mat$spr_rain) +
-                 mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
 
 summary(lm(env$sr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
              rain_mat$sum_rain + rain_mat$win_rain + rain_mat$spr_rain +
              mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB))
 
 anova(full, by='margin')
-anova(soil_ind, by='margin')
-anova(rain_ind, by='margin')
-anova(mang_ind, by='margin')
 
 print('RDA model of composition-------------------------------')
 ### rda model of composition
 full = rda(comm_sqr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
              rain_mat$sum_rain + rain_mat$win_rain + rain_mat$spr_rain +
              mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
-soil_ind = rda(comm_sqr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
-                 Condition(rain_mat$sum_rain) + Condition(rain_mat$win_rain) + Condition(rain_mat$spr_rain) +
-                 Condition(mang_mat$YrsOB) + Condition(mang_mat$BP5Yrs) + Condition(mang_mat$YrsSLB))
-rain_ind = rda(comm_sqr ~ 
-                 Condition(soil_mat[,1]) + Condition(soil_mat[,2]) + Condition(soil_mat[,3]) +
-                 rain_mat$sum_rain + rain_mat$win_rain + rain_mat$spr_rain +
-                 Condition(mang_mat$YrsOB) + Condition(mang_mat$BP5Yrs) + Condition(mang_mat$YrsSLB))
-mang_ind = rda(comm_sqr ~ 
-                 Condition(soil_mat[,1]) + Condition(soil_mat[,2]) + Condition(soil_mat[,3]) +
-                 Condition(rain_mat$sum_rain) + Condition(rain_mat$win_rain) + Condition(rain_mat$spr_rain) +
-                 mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
 
 anova(full, by='margin')
-anova(soil_ind, by='margin')
-anova(rain_ind, by='margin')
-anova(mang_ind, by='margin')
 
 print('CCA model of composition-------------------------------')
 ### cca model of composition
 full = cca(comm_sqr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
              rain_mat$sum_rain + rain_mat$win_rain + rain_mat$spr_rain +
              mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
-soil_ind = cca(comm_sqr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
-                 Condition(rain_mat$sum_rain) + Condition(rain_mat$win_rain) + Condition(rain_mat$spr_rain) +
-                 Condition(mang_mat$YrsOB) + Condition(mang_mat$BP5Yrs) + Condition(mang_mat$YrsSLB))
-rain_ind = cca(comm_sqr ~ 
-                 Condition(soil_mat[,1]) + Condition(soil_mat[,2]) + Condition(soil_mat[,3]) +
-                 rain_mat$sum_rain + rain_mat$win_rain + rain_mat$spr_rain +
-                 Condition(mang_mat$YrsOB) + Condition(mang_mat$BP5Yrs) + Condition(mang_mat$YrsSLB))
-mang_ind = cca(comm_sqr ~ 
-                 Condition(soil_mat[,1]) + Condition(soil_mat[,2]) + Condition(soil_mat[,3]) +
-                 Condition(rain_mat$sum_rain) + Condition(rain_mat$win_rain) + Condition(rain_mat$spr_rain) +
-                 mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
 
 anova(full, by='margin')
-anova(soil_ind, by='margin')
-anova(rain_ind, by='margin')
-anova(mang_ind, by='margin')
-
 
 ## examine for residual spatial dependence
 tgp_xy = env[ , c('easting', 'northing')]
@@ -95,12 +52,12 @@ ols_mso_temp = mso(ols_lm, env$yr, permutations = 999)
 rda_mso_temp = mso(rda_lm, env$yr, permutations = 999)
 cca_mso_temp = mso(cca_lm, env$yr, permutations = 999)
 
-pdf('./figs/repeat_model_mso.pdf', width=7, height=7*2)
-  par(mfrow=c(2,1))
-  msoplot(ols_mso_spat, main='Repeat, OLS, spatial')
-  msoplot(ols_mso_temp, main='Repeat, OLS, temporal')
-  msoplot(rda_mso_spat, main='Repeat, RDA, spatial')
-  msoplot(rda_mso_temp, main='Repeat, RDA, temporal')
-  msoplot(cca_mso_spat, main='Repeat, CCA, spatial')
-  msoplot(cca_mso_temp, main='Repeat, CCA, temporal')
+pdf('./figs/repeat_model_mso.pdf', width=7*2, height=7)
+  par(mfrow=c(1,2))
+  msoplot(ols_mso_spat, main='Repeat, OLS, spatial', ylim=c(50, 250))
+  msoplot(ols_mso_temp, main='Repeat, OLS, temporal', ylim=c(50, 225))
+  msoplot(rda_mso_spat, main='Repeat, RDA, spatial', ylim=c(10, 40))
+  msoplot(rda_mso_temp, main='Repeat, RDA, temporal', ylim=c(20, 35))
+  msoplot(cca_mso_spat, main='Repeat, CCA, spatial', ylim=c(1, 4))
+  msoplot(cca_mso_temp, main='Repeat, CCA, temporal', ylim=c(2, 3.5))
 dev.off()
