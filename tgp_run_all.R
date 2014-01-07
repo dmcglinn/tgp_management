204ae19155aab36f55146a73fc2c119b7e6b5e12
Daniel McGlinn <danmcglinn@gmail.com>
2014-01-07 13:45:02 -0700

    Add annotation to run_all script and require some of the scripts to be run linearly rather than in parallel.

--- tgp_run_all.R
+++ tgp_run_all.R
@@ -8,20 +8,25 @@ dir.create('./figs')
 
 ## perpare datasets ------------------------------------------------------------
 
+## download the climate data
 system('Rscript ./scripts/tgp_mesonet_download.R > ./scripts/log_files/mesonet_download.log 2>&1',
-       wait=FALSE)
+       wait=T)
 
+## filter enviornemntal data
 system('Rscript ./scripts/filter_enviornmental_data.R > ./scripts/log_files/filter_envio.log 2>&1',
-       wait=FALSE)
+       wait=T)
 
+## merge the various management datasets
 system('Rscript ./scripts/merge_management_data.R > ./scripts/log_files/merge_mang.log 2>&1',
-       wait=FALSE)
+       wait=T)
 
+## extract only the relevant management information
 system('Rscript ./scripts/extract_management_data.R > ./scripts/log_files/extract_mang.log 2>&1',
-       wait=FALSE)
+       wait=T)
 
+## generate a site-by-species matrix to analyze
 system('Rscript ./scripts/create_site_by_sp_matrix.R > ./scripts/log_files/site_by_sp.log 2>&1',
-       wait=FALSE)
+       wait=T)
 
 ## conduct analysis ------------------------------------------------------------
 ## variation partitioning 
@@ -42,6 +47,6 @@ system('Rscript ./scripts/tgp_grid_testpart.R > ./scripts/log_files/grid_testpar
 system('Rscript ./scripts/tgp_repeat_testpart.R > ./scripts/log_files/repeat_testpart.log 2>&1', 
        wait=FALSE)
 
-## make figures ----------------------------------------------------------------
+## make some of the figures ----------------------------------------------------
 system('Rscript ./scripts/make_maps.R')
 system('Rscript ./scripts/tgp_bison_partial_effect.R')
