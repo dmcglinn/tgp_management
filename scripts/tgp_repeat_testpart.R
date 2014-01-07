e093b21a6fa4dde1e8a41c4aaf8f34c8ebbcb88a
Daniel McGlinn <danmcglinn@gmail.com>
2014-01-07 11:09:04 -0700

    Replace analysis of specific environmental variables with analysis of site and year variables for Repeat analysis.

--- scripts/tgp_repeat_testpart.R
+++ scripts/tgp_repeat_testpart.R
@@ -1,66 +1,59 @@
 library(vegan)
 
-setwd('~/tgp_management/')
-
 source('./scripts/tgp_functions.R')
 source('./scripts/tgp_repeat_data_import.R')
 
 dir.create('./results/')
 
-rain_mat = as.data.frame(rain_mat)
 ## test significance of model terms
 print('OLS model of richness-----------------------------------')
 ## ols model of richness
-full = rda(env$sr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
-                    rain_mat$sum_rain + rain_mat$win_rain + rain_mat$spr_rain +
+full = rda(env$sr ~ plot_mat + year_mat + 
                     mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
 
-summary(lm(env$sr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
-             rain_mat$sum_rain + rain_mat$win_rain + rain_mat$spr_rain +
-             mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB))
+summary(lm(env$sr ~ plot_mat + year_mat + 
+                    mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB))
 
 anova(full, by='margin')
 
-soil = rda(env$sr, soil_mat, cbind(rain_mat, mang_mat))
-permutest(soil, permutations=999)
+site = rda(env$sr, plot_mat, cbind(year_mat, mang_mat))
+permutest(site, permutations=999)
 
-rain = rda(env$sr, rain_mat, cbind(soil_mat, mang_mat))
-permutest(rain, permutations=999)
+year = rda(env$sr, year_mat, cbind(plot_mat, mang_mat))
+permutest(year, permutations=999)
 
-mang = rda(env$sr, mang_mat, cbind(rain_mat, soil_mat))
+mang = rda(env$sr, mang_mat, cbind(year_mat, plot_mat))
 permutest(mang, permutations=999)
 
 print('RDA model of composition-------------------------------')
 ### rda model of composition
-full = rda(comm_sqr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
-             rain_mat$sum_rain + rain_mat$win_rain + rain_mat$spr_rain +
-             mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
+full = rda(comm_sqr ~ plot_mat + year_mat +
+                      mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
 anova(full, by='margin')
 
-soil = rda(comm_sqr, soil_mat, cbind(rain_mat, mang_mat))
-permutest(soil, permutations=999)
+site = rda(comm_sqr, plot_mat, cbind(year_mat, mang_mat))
+permutest(site, permutations=999)
 
-rain = rda(comm_sqr, rain_mat, cbind(soil_mat, mang_mat))
-permutest(rain, permutations=999)
+year = rda(comm_sqr, year_mat, cbind(plot_mat, mang_mat))
+permutest(year, permutations=999)
 
-mang = rda(comm_sqr, mang_mat, cbind(rain_mat, soil_mat))
+mang = rda(comm_sqr, mang_mat, cbind(year_mat, plot_mat))
 permutest(mang, permutations=999)
 
 
 print('CCA model of composition-------------------------------')
 ### cca model of composition
-full = cca(comm_sqr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
-             rain_mat$sum_rain + rain_mat$win_rain + rain_mat$spr_rain +
-             mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
+full = cca(comm_sqr ~ plot_mat + year_mat +
+                      mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
 anova(full, by='margin')
 
-soil = cca(comm_sqr, soil_mat, cbind(rain_mat, mang_mat))
-permutest(soil, permutations=999)
+site = cca(comm_sqr, plot_mat, cbind(year_mat, mang_mat))
+permutest(site, permutations=999)
 
-rain = cca(comm_sqr, rain_mat, cbind(soil_mat, mang_mat))
-permutest(rain, permutations=999)
+year = cca(comm_sqr, year_mat, cbind(plot_mat, mang_mat))
+permutest(year, permutations=999)
 
-mang = cca(comm_sqr, mang_mat, cbind(rain_mat, soil_mat))
+mang = cca(comm_sqr, mang_mat, cbind(year_mat, plot_mat))
 permutest(mang, permutations=999)
 
 
