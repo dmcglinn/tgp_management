b4010422347544bae6cfb80b09c442fd27afc37c
Dan McGlinn <danmcglinn@gmail.com>
2018-11-20 09:56:42 -0500

    Add publication quality partial regression and ordination plots to repeat and grid analyses

--- scripts/tgp_grid_testpart.R
+++ scripts/tgp_grid_testpart.R
@@ -28,28 +28,42 @@ Anova(full)
 summary(full)
 avPlots(full)
 
-dat= data.frame(sr=env$sr, soil_mat, mang_mat)
+dat = data.frame(sr=env$sr, soil_mat, mang_mat)
 full = lm(sr ~ ., data=dat)
+full_std = lm(sr ~ . , data = data.frame(scale(dat)))
+full_stats = data.frame(summary(full_std)$coefficients)
+names(full_stats) = c('beta', 'se', 't', 'p')
 
-png('./figs/grid_partial_regression.png', width=480*3, height=480*2,
-    res=100)
+# fix up the p-values 
+full_stats$p = round(full_stats$p, 3)
+full_stats$p[2] = " < 0.001"
+full_stats$p[-2] = paste(" =", full_stats$p[-2])
+
+
+pdf('./figs/grid_partial_regression.pdf', width=7*2, height=7*1.5)
 par(mfrow=c(2,3))
 ylabs = c('Soil PC1', 'Soil PC2' , 'Soil PC3', 
           'Years of Bison', '# of Burns in Past 5 Years', 'Years Since Burn')
 for(i in seq_along(ylabs)) {
     termplot(full, partial.resid = T, terms=names(dat)[i + 1],
-             se=T, smooth=panel.smooth, col.res = 'black',
-             col.term = 'blue', lwd.term=2,
-             col.se = 'blue', lwd.se = 2, lty.se=3,
+             se=T, smooth=panel.smooth, pch=19, cex=0.75,
+             col.res = 'black', col.term = 'dodgerblue', lwd.term=2,
+             col.se = 'dodgerblue', lwd.se = 2, lty.se=3,
              col.smth = 'red', lty.smth = 2,
-             xlabs='', ylabs='', axes=F, bty='n')
+             xlabs='', ylabs='', axes=F, bty='n', ylim=c(-32, 40))
+    stats = substitute(paste(beta, ' = ', beta_std, ", ", italic(p), pv), 
+                     list(beta_std = round(full_stats$beta[i + 1], 2),
+                          pv = full_stats$p[i + 1]))
+    legend('bottomleft', legend=stats, bty='n', cex=2)
     axis(side=1, cex.axis=1.75, padj=0.5)
     axis(side=2, cex.axis=1.75)
     mtext(side=1, ylabs[i], padj=2.5, cex=1.5)
     mtext(side=2, "Partial Residuals", padj=-1.75, cex=1.5)
+    mtext(side=3, paste(letters[i], ")", sep=''), cex=1.5, 
+          at = par("usr")[1]+0.025*diff(par("usr")[1:2]))
     if (i == 3)
         legend('topright', c('model fit', '95% CI', 'lowess line'), 
-               col=c('blue','grey', 'red'), lty=c(1,1,2), lwd=c(2, 8, 2),
+               col=c('dodgerblue','grey', 'red'), lty=c(1,1,2), lwd=c(2, 8, 2),
                cex=2, bty='n')
 }
 dev.off()
@@ -76,16 +90,74 @@ dev.off()
 
 
 print('RDA model of composition-------------------------------')
-### rda model of composition
+### rda model of composition --------------------
 full = rda(comm_sqr ~ soil_mat[,1] + soil_mat[,2] + soil_mat[,3] +
              mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
 anova(full, by='margin')
 
-soil = rda(comm_sqr, soil_mat, mang_mat)
+soil = rda(comm_sqr ~ soil_mat$Comp.1 + soil_mat$Comp.2 + soil_mat$Comp.3 +
+           Condition(mang_mat$YrsOB) + Condition(mang_mat$BP5Yrs) +
+           Condition(mang_mat$YrsSLB))
 permutest(soil, permutations=999)
-
+u
 mang = rda(comm_sqr, mang_mat, soil_mat)
 permutest(mang, permutations=999)
+
+sp_scr = soil$CCA$v[ , 1:2]
+sp_core = c('andrgera', 'sorgnuta', 'panivirg')
+indices = which(row.names(sp_scr) %in% sp_core)
+
+
+rda_stat = list(list(R2 = RsquareAdj(soil)), list())
+
+pdf('./figs/grid_rda_partial.pdf', width=7*1.5, height=7)
+par(mfrow=c(1, 2))
+
+xlim = c(-4, 3)
+ylim = c(-3, 3)
+plot(soil, type='n', scaling=1, axes=F, xlim=xlim, ylim=ylim,
+     xlab='', ylab='')
+text(soil, display='bp', labels=rep('', 3), col='red', lwd=2)
+orditorp(soil, display='sp', cex=0.75, scaling=1,
+         pcol='dodgerblue', pch=19)
+points(sp_scr[indices, 1], sp_scr[indices, 2], pch=2, col='blue')
+text(sp_scr[indices, 1], sp_scr[indices, 2], labels = sp_core, col='purple') 
+x = c(2.85, 0.69, 0.55)
+y = c(0.25, -2.5, 1.1)
+text(x, y, labels = paste0('PC', 1:3), col='red', lwd=2)
+axis(side=1, cex.axis=1.5, padj=0.5)
+axis(side=2, cex.axis=1.5)
+mtext(side=1, 'partial RDA axis 1', cex=1.5, padj=3.5)
+mtext(side=2, 'partial RDA axis 2', cex=1.5, padj=-3)
+mtext(side=3, "a)", cex=1.5, 
+      at = par("usr")[1]+0.025*diff(par("usr")[1:2]))
+stats = substitute(paste(italic(R^2), ' = ', r2, ", ", italic(p), ' = ', pv), 
+                   list(r2 = round(lm_stat[[i]]$r.squared, 2),
+                        pv = 1 - round(pf(lm_stat[[i]]$f[1], 
+                                          lm_stat[[i]]$f[2],
+                                          lm_stat[[i]]$f[3]), 3))) 
+legend('bottomleft', legend=stats, bty='n', cex=2)
+
+
+##
+plot(mang, type='n', scaling=1, axes=F, xlim=xlim, ylim=ylim,
+     xlab='', ylab='')
+text(mang, display='bp', labels = rep('', 3), col='red', lwd=2)
+orditorp(mang, display='sp', cex=0.75, scaling=1,
+         pcol='dodgerblue', pch=19)
+x = c(-1.6, 2.33, -3.05)
+y = c(3.5, -2.45, -1.35)
+text(x, y, 
+     labels = c('Years\nof Bison', '# Burns Past\n5 years', 
+                'Years Since Burn'),
+     col='red', lwd=2)
+axis(side=1, cex.axis=1.5, padj=0.5)
+axis(side=2, cex.axis=1.5)
+mtext(side=1, 'partial RDA axis 1', cex=1.5, padj=3.5)
+mtext(side=2, 'partial RDA axis 2', cex=1.5, padj=-3)
+mtext(side=3, "b)", cex=1.5, 
+      at = par("usr")[1]+0.025*diff(par("usr")[1:2]))
+dev.off()
 
 print('CCA model of composition-------------------------------')
 ### cca model of composition
--- scripts/tgp_repeat_testpart.R
+++ scripts/tgp_repeat_testpart.R
@@ -5,11 +5,14 @@ source('./scripts/tgp_functions.R')
 source('./scripts/tgp_repeat_data_import.R')
 source('./scripts/termplot.R')
 
+env$plot = as.factor(env$plot)
+env$yr = as.factor(env$yr)
+
 dir.create('./results/')
 
 ## test significance of model terms
 print('OLS model of richness-----------------------------------')
-## ols model of richness
+## ols model of richness-------------
 print('non-parametric tests')
 full = rda(env$sr ~ plot_mat + year_mat + 
                     mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
@@ -25,33 +28,67 @@ mang = rda(env$sr, mang_mat, cbind(year_mat, plot_mat))
 permutest(mang, permutations=999)
 
 print('parametric tests')
-full = lm(env$sr ~ as.factor(env$plot) + as.factor(env$yr) + 
+plotID = factor(env$plot)
+year = as.factor(env$yr)
+full = lm(env$sr ~ plotID + year + 
           mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
+
+plotID_res = residuals(full, 'partial')[ , 1]
+year_res = residuals(full, 'partial')[ , 2]
+ylim = range(plotID_res, year_res)
+#ID = factor(ID, levels(ID)[order(tapply(ID_res, ID, mean))])
+pdf('./figs/repeat_partial_regression_factors.pdf', width=7*1.33)
+xlabs = c('Plot ID', 'Year')
+par(mfrow=c(1,2))
+#for(i in seq_along(xlabs)) {
+    boxplot(plotID_res ~ plotID, frame.plot=F, ylim=ylim)
+    boxplot(year_res ~ year, frame.plot=F, ylim=ylim)
+#    axis(side=1, labels= cex.axis=1.75, padj=0.5)
+#    axis(side=2, cex.axis=1.75)
+#    mtext(side=1, xlabs[i], padj=2.5, cex=1.5)
+#    mtext(side=2, "Partial Residuals", padj=-1.75, cex=1.5)
+#}
+dev.off()
+
 Anova(full)
 summary(full)
 avPlots(full)
 
-dat= data.frame(sr=env$sr, plot_mat, year_mat, mang_mat)
+dat = data.frame(sr=env$sr, plot_mat, year_mat, mang_mat)
 full = lm(sr ~ ., data=dat)
+full_std = lm(sr ~ . , data = data.frame(scale(dat)))
+full_stats = data.frame(summary(full_std)$coefficients)
+names(full_stats) = c('beta', 'se', 't', 'p')
 
-png('./figs/repeat_partial_regression.png', width=480*3, height=480*1,
-    res=100)
-par(mfrow=c(1,3))
-ylabs = c('Years of Bison', '# of Burns in Past 5 Years', 'Years Since Burn')
-for(i in seq_along(ylabs)) {
+# fix up the p-values 
+full_stats$p[32] = " < 0.001"
+full_stats$p[33] = " = 0.01"
+full_stats$p[34] = " = 0.001"
+
+
+pdf('./figs/repeat_partial_regression.pdf', width = 7*2, height = 7*1)
+par(mfrow = c(1, 3))
+xlabs = c('Years of Bison', '# of Burns in Past 5 Years', 'Years Since Burn')
+for(i in seq_along(xlabs)) {
     termplot(full, partial.resid = T, terms=names(dat)[i + 31],
              se=T, smooth=panel.smooth, col.res = 'black',
-             col.term = 'blue', lwd.term=2,
-             col.se = 'blue', lwd.se = 2, lty.se=3,
+             col.term = 'dodgerblue', lwd.term=2,
+             col.se = 'dodgerblue', lwd.se = 2, lty.se=3,
              col.smth = 'red', lty.smth = 2,
-             xlabs='', ylabs='', axes=F, bty='n')
+             xlabs='', ylabs='', axes=F, bty='n', ylim = c(-25, 25))
+    stats = substitute(paste(beta, ' = ', beta_std, ", ", italic(p), pv), 
+                  list(beta_std = round(full_stats$beta[i + 31], 2),
+                       pv = full_stats$p[i + 31]))
+    legend('bottomleft', legend=stats, bty='n', cex=2)
     axis(side=1, cex.axis=1.75, padj=0.5)
     axis(side=2, cex.axis=1.75)
-    mtext(side=1, ylabs[i], padj=2.5, cex=1.5)
+    mtext(side=1, xlabs[i], padj=2.5, cex=1.5)
     mtext(side=2, "Partial Residuals", padj=-1.75, cex=1.5)
+    mtext(side=3, paste(letters[i], ")", sep=''), cex=1.5, 
+          at = par("usr")[1] + 0.025*diff(par("usr")[1:2]))
     if (i == 3)
         legend('topright', c('model fit', '95% CI', 'lowess line'), 
-               col=c('blue','grey', 'red'), lty=c(1,1,2), lwd=c(2, 8, 2),
+               col=c('dodgerblue','grey', 'red'), lty=c(1,1,2), lwd=c(2, 8, 2),
                cex=2, bty='n')
 }
 dev.off()
@@ -75,7 +112,7 @@ abline(h=0, col='grey', lty=2, lwd=2)
 dev.off()
 
 print('RDA model of composition-------------------------------')
-### rda model of composition
+### rda model of composition------------
 full = rda(comm_sqr ~ plot_mat + year_mat +
                       mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
 anova(full, by='margin')
@@ -92,37 +129,91 @@ mang = rda(comm_sqr ~ Condition(plot) + Condition(yr) + YrsOB + BP5Yrs + YrsSLB,
            data=env)
 permutest(mang, permutations=999)
 
-### plot RDA partial plots
-png('./figs/repeat_ordination.png', res=100)
-par(mfrow=c(1,3))
-xlims = c(-1.5, 1.5)
-ylims = c(-2, 2)
-plot(site, type='n', xlim=xlims, ylim=ylims)
-points(site, 'sp', pch=19, col='dodgerblue', cex=1)
-points(site, 'cn', pch=2, col='red')
-plot(year, type='n', xlim=xlims, ylim=ylims)
-points(year, 'sp', pch=19, col='dodgerblue', cex=1)
-points(year, 'cn', pch=2, col='red')
-text(year, 'cn', 1998:2009, col='red')
-plot(mang, type='n', xlim=xlims, ylim=ylims)
-points(mang, 'sp', pch=19, col='dodgerblue', cex=1)
-text(mang, 'bp', col='red', cex=1, label=c('YS', 'BP5Y','YB'))
+### plot RDA partial plots--------
+
+pdf('./figs/repeat_rda.pdf', width=7*1.5)
+par(mfrow=c(1,2))
+xlims = c(-3.75, 5.75)
+ylims = c(-6, 4)
+results = list(site, year, mang)
+yr_labels = substr(as.character(1998:2009), 3,4)
+yr_cols = colorRampPalette(c('black', 'red'))
+for (i in seq_along(results)) {
+    plot(results[[i]], type='n', scaling=1, axes=F, xlab='', ylab='', 
+         xlim=xlims, ylim=ylims)
+    if (i == 1) {
+        orditorp(results[[i]], 'sp', cex=0.75, scaling=1, col=1,
+               pcol='dodgerblue', pch=19)  
+        points(results[[i]], 'cn', pch=2, col='red', cex=1.5)
+    } 
+    if (i == 2) { 
+        orditorp(results[[i]], 'sp', cex=0.75, scaling=1, col=1,
+               pcol='dodgerblue', pch=19)        
+        points(year, 'cn', pch=2, col='red', cex=1.5)
+    } 
+    if (i == 3) {
+        orditorp(results[[i]], 'sp', cex=0.75, scaling=1, col=1,
+                 pcol='dodgerblue', pch=19) 
+        text(mang, 'bp', lwd=2, 
+             labels = c('Years of Bison', '# Burns Past 5 years',
+                        'Years Since Burn'),
+             col='red', cex=1, axis.bp = FALSE)
+ 
+    }
+    axis(side=1, cex.axis=1.5, padj=0.5)
+    axis(side=2, cex.axis=1.5)
+    mtext(side=1, 'partial RDA axis 1', cex=1.5, padj=3.5)
+    mtext(side=2, 'partial RDA axis 2', cex=1.5, padj=-3)
+    mtext(side=3, paste(letters[i], ")", sep=''), cex=1.5, 
+          at = par("usr")[1]+0.025*diff(par("usr")[1:2]))
+}
 dev.off()
 
+
 print('CCA model of composition-------------------------------')
-### cca model of composition
+### cca model of composition------------
 full = cca(comm_sqr ~ plot_mat + year_mat +
                       mang_mat$YrsOB + mang_mat$BP5Yrs + mang_mat$YrsSLB)
 anova(full, by='margin')
 
-site = cca(comm_sqr, plot_mat, cbind(year_mat, mang_mat))
+site = cca(comm_sqr ~ plot + Condition(yr) + Condition(YrsOB) + Condition(BP5Yrs) +
+           Condition(YrsSLB), data=env)
 permutest(site, permutations=999)
 
-year = cca(comm_sqr, year_mat, cbind(plot_mat, mang_mat))
+year = cca(comm_sqr ~ Condition(plot) + yr + Condition(YrsOB) + Condition(BP5Yrs) +
+           Condition(YrsSLB), data=env)
 permutest(year, permutations=999)
 
-mang = cca(comm_sqr, mang_mat, cbind(year_mat, plot_mat))
+mang = cca(comm_sqr ~ Condition(plot) + Condition(yr) + YrsOB + BP5Yrs + YrsSLB,
+           data=env)
 permutest(mang, permutations=999)
 
+pdf('./figs/repeat_cca.pdf', width=7*1.5)
+par(mfrow=c(1,2))
+xlims = c(-15, 15)
+ylims = c(-15, 15)
+results = list(site, year, mang)
+yr_labels = substr(as.character(1998:2009), 3,4)
+yr_cols = colorRampPalette(c('black', 'red'))
+for (i in seq_along(results)) {
+    plot(results[[i]], type='n', scaling=1, axes=F, xlab='', ylab='', 
+         xlim=xlims, ylim=ylims)
+    orditorp(results[[i]], 'sp', cex=0.75, scaling=1, col=1,
+             pcol='dodgerblue', pch=19)
+    if (i == 1) 
+        points(results[[i]], 'cn', pch=2, col='red', cex=1.5)
+    if (i == 2)
+        points(year, 'cn', pch=2, col='red', cex=1.5)
+    if (i == 3)
+        text(mang, 'bp',
+             labels = c('Years of Bison', '# Burns Past 5 years',
+                        'Years Since Burn'),
+             col='red', cex=1, axis.bp = FALSE)
+    axis(side=1, cex.axis=1.5, padj=0.5)
+    axis(side=2, cex.axis=1.5)
+    mtext(side=1, 'partial CCA axis 1', cex=1.5, padj=3.5)
+    mtext(side=2, 'partial CCA axis 2', cex=1.5, padj=-3)
+}
+dev.off()
 
 
